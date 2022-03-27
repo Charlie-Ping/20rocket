1a00b0ec2641671db1a7138a4ca30c286053f5b9
Charlie-Ping <1758489207@qq.com>
2022-03-27 18:29:34 +0800

    增加加速度传感器

--- rocket/arduino/main/main.ino
+++ rocket/arduino/main/main.ino
@@ -1,29 +1,70 @@
+#include "Wire.h"
+
+#include "I2Cdev.h"
+#include "MPU6050.h"
+
 #include <SD.h>
 
+MPU6050 carsicker;
+
+
+#define LED_PIN 13
+bool blinkState = false;
+int16_t ax, ay, az;
+int16_t gx, gy, gz;
 
 const int csPin = 10;
 const int airDigitalPin = 9;
 const int airAnalogPin = 0;
+bool sd_ok;
+bool mpu_ok;
 
 
-bool testSD() {
-  bool sd_ok;
+void testSD() {
   sd_ok = SD.begin(csPin);
-  if (!sd_ok) {
-    Serial.println("Error: load SD crashed");
-    return false;
-  } else { 
-    Serial.println("Load SD OK"); 
-    return true;
-  }
+  //  一般情况下仅仅测试是会返回true, 但是不注释掉写入等操作的代码就出问题了
+  Serial.println(sd_ok ? "SD testing success" : "SD crashed!");
+}
+
+void testMPU() {
+  Serial.println("init MPU...");
+  carsicker.initialize();
+  Serial.println("connect MPU...");
+  mpu_ok = carsicker.testConnection();
+  Serial.println(mpu_ok ? "mpu testing success" : "mpu crashed!");
+}
+
+void setup() {
+  Wire.begin();
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  testSD();
+  testMPU();
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  int air_analog = readAirQuality();
+  
+  Serial.println("Air Quality: " + air_analog);
+  carsicker.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
+  Serial.print("a/g:\t");
+  Serial.print(ax); Serial.print("\t");
+  Serial.print(ay); Serial.print("\t");
+  Serial.print(az); Serial.print("\t");
+  Serial.print(gx); Serial.print("\t");
+  Serial.print(gy); Serial.print("\t");
+  Serial.println(gz);
+  delay(250);
+  
+}
+
+int readAirQuality() {
+  int air_analog = analogRead(airAnalogPin);
+  return air_analog;
 }
 
 
-void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(9600);
-
-  
 //  Serial.println("loading SD");
 //  bool sd_ok;
 //  sd_ok = false;
@@ -46,34 +87,3 @@ void setup() {
 //  Log = SD.open("log.txt", FILE_WRITE);
 //  Serial.println(Log.read());
 //  Log.close();
-}
-
-void loop() {
-  // put your main code here, to run repeatedly:
-  int air_analog = analogRead(airAnalogPin);
-  
-  Serial.println(air_analog);
-  delay(250);
-}
-
-// official demo code
-//void printDirectory(File dir, int numTabs) {
-//  while (true) {
-//    File entry =  dir.openNextFile();
-//    if (! entry) {
-//      break;
-//    }
-//    for (uint8_t i = 0; i < numTabs; i++) {
-//      Serial.print('\t');
-//    }
-//    Serial.print(entry.name());
-//    if (entry.isDirectory()) {
-//      Serial.println("/");
-//      printDirectory(entry, numTabs + 1);
-//    } else {
-//      Serial.print("\t\t");
-//      Serial.println(entry.size(), DEC);
-//    }
-//    entry.close();
-//  }
-//}
